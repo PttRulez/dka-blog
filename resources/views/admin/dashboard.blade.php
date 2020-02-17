0d1807ab2c068ee54834da6cdfadb8bc07075b04
petya <sashafem@mail.ru>
2020-02-17 03:35:30 +0300

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,46 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Категорий 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Материалов 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Посетителей 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="badge badge-primary">Сегодня 0</span></p>
+                </div>
+            </div>
+        </div><!-- row -->
+
+        <div class="row">
+            <div class="col-sm-6 list-group">
+                <a href="#" class="btn btn-block btn-outline-secondary">Создать категорию</a>
+                <a href="" class="list-group-item list-group-item-action">
+                    <h4 class="list-group-item-heading">Категория первая</h4>
+                    <p class="list-group-item-text">Кол-во материалов</p>
+                </a>
+            </div>
+
+            <div class="col-sm-6 list-group">
+                <a href="#" class="btn btn-block btn-outline-secondary">Создать материал</a>
+                <a href="" class="list-group-item list-group-item-action">
+                    <h4 class="list-group-item-heading">Материал первый</h4>
+                    <p class="list-group-item-text">Категория</p>
+                </a>
+            </div>
+        </div><!-- row -->
+    </div>
+@endsection
